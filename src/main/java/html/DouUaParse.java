9abd8ee901ff7273d9539d56b02a1d0e13c02523
Sekator778 <sekator778@gmail.com>
2020-09-06 13:49:28 +0300

    dom

--- src/main/java/html/DouUaParse.java
+++ src/main/java/html/DouUaParse.java
@@ -6,7 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
- *
+ * Document Object Model
  */
 
 public class DouUaParse {
@@ -14,15 +14,14 @@ public class DouUaParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://jobs.dou.ua/vacancies/?category=Java").get();
         Elements row = doc.select(".title");
-        Element href = null;
-        Element cities = null;
+        Element href;
+        Element cities;
         for (Element td : row) {
-                href = td.child(0);
-                cities = td.child(2);
+            href = td.child(0);
+            cities = td.child(2);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
             System.out.println(cities.getAllElements().text());
-
         }
     }
 }
